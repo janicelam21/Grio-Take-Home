3333313e8bd58f11d09d4edfd2f6047fe5351eb4
Janice Lam <janice.lam21@gmail.com>
2019-10-04 11:52:07 -0700

    modify project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,21 @@
 module.exports = {
-    entry: __dirname + '/client/index.jsx',
-    module: {
-      output: {
-        filename: 'bundle.js',
-        path: __dirname + '/client/dist',
-        publicPath: '/'
-      },
-      rules: [
-        { 
-          test: [/\.jsx$/],
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env']
-            }
+  entry: __dirname + '/client/index.jsx',
+  module: {
+    rules: [
+      { 
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
           }
         }
-      ]
-    },
-    devServer: {
-      historyApiFallback: true,
-    }
-  };
+      }
+    ]
+  },
+   output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
+};
