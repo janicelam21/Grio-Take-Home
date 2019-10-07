1b9576971b56da454cee7ca783fd782d20a20207
Janice Lam <janice.lam21@gmail.com>
2019-10-07 07:10:09 -0700

    update status codes

--- server/routes.js
+++ server/routes.js
@@ -4,9 +4,10 @@ const auth = require('./auth.js');
 const postAuth = (req, res) => {
   if (req.body.username === fakeUser.username && req.body.password === fakeUser.password) {
     const token = auth.token(req.body.username);
+    res.status(200);
     res.json({token: token});
   } else {
-    res.send({error: 'Wrong username or password'});
+    res.status(401).send({error: 'Wrong username or password'});
   }
 }
 
@@ -14,7 +15,7 @@ const postCalc = (req, res) => {
   var currCount = req.body.count;
   auth.verifyToken(req.token, (err, data) => {
     if (err) {
-      res.status(403).send(err);
+      res.status(401).send(err);
     } else {
       var increment = Math.max(1, currCount*2);
       res.status(200).send({nextCount: increment});
